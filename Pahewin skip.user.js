00a8c45afbe903249bf7cbded5512bd99b903944
Weegee30 <124709771+Weegee30@users.noreply.github.com>
2023-06-27 22:37:37 -0400

    Update Pahewin skip.user.js

--- Pahewin skip.user.js
+++ Pahewin skip.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Pahewin skip
+// @name         animepahe Pahewin skip
 // @namespace    https://animepahe.ru
 // @version      0.1
 // @description  Tired of waiting to redirect to download from animepahe? Use this script. It's literally just one line of code.
-// @author       Weegee
+// @author       Weegee30
 // @match        https://pahe.win/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=animepahe.ru
 // @grant        none
